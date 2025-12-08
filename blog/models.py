4050d128354a41cb6860ad2c5de3decdd169dc28
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2025-12-08 17:07:11 +0000

    Create About app and test

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,14 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 # Create your models here.
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)

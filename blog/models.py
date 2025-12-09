8234c190f2f1d2a9e8530ddf87e33a9a5e6d4fc4
Lyd-W <209948061+Lyd-W@users.noreply.github.com>
2025-12-09 15:20:29 +0000

    Add and configure Django AllAuth with navigation

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,7 @@ class About(models.Model):
     def __str__(self):
         return self.title
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
